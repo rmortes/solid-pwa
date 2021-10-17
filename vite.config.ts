22bb582693ef6d06f5ef3183e017380dbcb00f97
Raúl Mortes <correomortes@gmail.com>
2021-10-17 23:28:19 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import WindiCSS from "vite-plugin-windicss";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    solidPlugin(),
+    WindiCSS(),
+    VitePWA({
+      includeAssets: ["robots.txt"],
+      workbox: {
+        globPatterns: [
+          "**\/*.{js,css,html}",
+          "manifest.webmanifest",
+        ],
+      },
+      manifest: {
+        name: 'Solid app',
+        short_name: 'Solid app',
+        description: 'Solid app',
+        theme_color: '#ffffff',
+        // icons: [
+        //   {
+        //     src: 'pwa-192x192.png',
+        //     sizes: '192x192',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'pwa-512x512.png',
+        //     sizes: '512x512',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'pwa-512x512.png',
+        //     sizes: '512x512',
+        //     type: 'image/png',
+        //     purpose: 'any maskable',
+        //   }
+        // ]
+      }
+    })
+  ],
+  build: {
+    target: "esnext",
+    polyfillDynamicImport: false,
+  },
+});
